d5fc350613b0ff66e51f2d2e647fb01e8ea6034d
YuChia <h42279@gmail.com>
2023-10-12 02:20:56 +0800

    更新 nuget 套件、移除 Swashbuckle.AspNetCore.Newtonsoft

--- src/lab.swagger/Infrastructure/ServiceCollectionExtension/SwaggerSettingExtension.cs
+++ src/lab.swagger/Infrastructure/ServiceCollectionExtension/SwaggerSettingExtension.cs
@@ -29,7 +29,5 @@ public static class SwaggerSettingExtension
                 options.IncludeXmlComments(xmlFile);
             }
         });
-
-        services.AddSwaggerGenNewtonsoftSupport();
     }
 }
