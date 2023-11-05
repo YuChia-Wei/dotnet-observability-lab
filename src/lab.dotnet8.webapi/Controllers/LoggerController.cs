fa659101380c226fdcdc472a42c9e0b3e5e71e0b
YuChia <h42279@gmail.com>
2023-11-05 09:51:28 +0800

    add dotnet 8 api project

---
+++ src/lab.dotnet8.webapi/Controllers/LoggerController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab.dotnet8.webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoggerController : ControllerBase
+{
+    private readonly ILogger<LoggerController> _logger;
+
+    public LoggerController(ILogger<LoggerController> logger)
+    {
+        this._logger = logger;
+    }
+
+    [HttpPost("exception")]
+    public Task<IActionResult> ExceptionSample()
+    {
+        this._logger.LogInformation("Throw Exception");
+
+        throw new Dotnet7Exception();
+    }
+
+    /// <summary>
+    /// Send all level log.
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("send")]
+    public Task<IActionResult> SendAsync()
+    {
+        this._logger.LogCritical("Log Critical");
+        this._logger.LogDebug("Log Debug");
+        this._logger.LogError("Log Error");
+        this._logger.LogTrace("Log Trace");
+        this._logger.LogWarning("Log Warning");
+        this._logger.LogInformation("Log Information");
+
+        return Task.FromResult<IActionResult>(this.Ok());
+    }
+}
+
+public class Dotnet7Exception : Exception
+{
+}
